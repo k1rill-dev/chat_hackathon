509f56a7a0d3dab114edf9291910f902afa4c1a4
Decricher <55853487+pavelb22@users.noreply.github.com>
2023-03-18 23:17:14 +0300

    по мелочи подровнял

--- static/js/chat.js
+++ static/js/chat.js
@@ -9,6 +9,7 @@ const socket = new WebSocket(
     + '/'
 );
 
+
 socket.onopen = function(e){
     console.log("CONNECTION ESTABLISHED");
 }
@@ -23,14 +24,17 @@ socket.onerror = function(e){
 
 socket.onmessage = function(e){
     const data = JSON.parse(e.data);
+    let date = new Date();
     if(data.username == message_username){
         document.querySelector('#chat-body').innerHTML += `<tr>
                                                                 <td>
                                                                 <p class="bg-success p-2 mt-2 mr-5 shadow-sm text-white float-right rounded">${data.message}</p>
                                                                 </td>
+                                                                <td>
+                                <p><small class="p-1 shadow-sm">${date.getHours()}:${date.getMinutes()}</small>
+                                </p>
+                            </td>
                                                             </tr>`
-
-
     }else{
     if(data.type_msg == 'file') {
                     document.querySelector('#chat-body').innerHTML += `<tr>
@@ -39,22 +43,53 @@ socket.onmessage = function(e){
       <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-file-earmark" viewBox="0 0 16 16">
   <path d="M14 4.5V14a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h5.5L14 4.5zm-3 0A1.5 1.5 0 0 1 9.5 3V1H4a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V4.5h-2z"/>
 </svg> ${data.message}</a>
-  </td>                                                          </tr>`
-
-
-
-
-
+  </td> <td><p><small class="p-1 shadow-sm">${date.getHours()}:${date.getMinutes()}</small></p></td></tr>`
 
     }else{
         document.querySelector('#chat-body').innerHTML += `<tr>
                                                                 <td>
                                                                 <p class="bg-primary p-2 mt-2 mr-5 shadow-sm text-white float-left rounded">${data.message}</p>
                                                                 </td>
+                                                                <td>
+                                <p><small class="p-1 shadow-sm">${date.getHours()}:${date.getMinutes()}</small>
+                                </p>
+                            </td>
                                                             </tr>`
          }
     }
 }
+
+
+
+
+
+function isValidHttpUrl(string) {
+  let url;
+  try {
+    url = new URL(string);
+  } catch (_) {
+    return false;
+  }
+  return url.protocol === "http:" || url.protocol === "https:";
+}
+
+const input = document.getElementById('message_input');
+input.addEventListener('change', (event) => {
+  if(isValidHttpUrl(input.value)){
+  var sogl = confirm("Прикрепляя ссылку на сторонний ресурс вы полность несете ответственноть за любые возможные негативные последствия");
+  if(sogl){}
+  else{
+  input.value = '';
+  }
+  }
+
+})
+
+
+
+
+
+
 
 document.querySelector('#chat-message-submit').onclick = function(e){
     const message_input = document.querySelector('#message_input');
@@ -70,9 +105,29 @@ document.querySelector('#chat-message-submit').onclick = function(e){
     message_input.value = '';
 }
 
+
+
+input.addEventListener('change', (event) => {
+  const target = event.target
+  	if (target.files && target.files[0]) {
+
+      /*Maximum allowed size in bytes
+        5MB Example
+        Change first operand(multiplier) for your needs*/
+      const maxAllowedSize = 2 * 1024 * 1024;
+      if (target.files[0].size > maxAllowedSize) {
+      	alert('File size over 20MB, please select another');
+       	target.value = ''
+      }
+  }
+})
+
+
 document.querySelector('#chat-file-submit').onclick = function(e){
 
     const message_input = document.querySelector('#id_file');
+
+
     const message = message_input.value;
     var new_str = message.split('\\').pop();
 
