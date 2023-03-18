ec6f96de5ace5a6c2d5317e0ac20821c7c5a91c5
kirill-dev <userkirill609@gmail.com>
2023-03-18 19:58:51 +0300

    add update password and profile picture

--- main/forms.py
+++ main/forms.py
@@ -9,6 +9,9 @@ class LoginForm(forms.Form):
 
 class UpdateUserForm(forms.Form):
     avatar = forms.ImageField(widget=forms.FileInput, required=False, label='Фото профиля')
-    old_passwd = forms.CharField(widget=forms.PasswordInput, label='Текущий пароль')
-    passwd = forms.CharField(widget=forms.PasswordInput, label='Новый пароль')
-    passwd1 = forms.CharField(widget=forms.PasswordInput, label='Подтвердите пароль')
+    old_passwd = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': "Текущий пароль"}),
+                                 label='Текущий пароль', required=False)
+    passwd = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': "Новый пароль"}), label='Новый пароль',
+                             required=False)
+    passwd1 = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': "Повторите пароль"}),
+                              label='Подтвердите пароль', required=False)
--- main/views.py
+++ main/views.py
@@ -1,6 +1,4 @@
 from django.contrib.auth.hashers import check_password
-
-from chats import Aes
 from .models import User
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
@@ -16,42 +14,44 @@ def index_main(request):
     return render(request, 'main/main.html', {'request': request, 'user': user})
 
 
-# TODO: смена пароля, аватара
 def view_my_profile(request):
     user = User.objects.get(id=request.user.id)
     if request.method == 'POST':
         form = UpdateUserForm(request.POST, request.FILES)
         if form.is_valid():
             cd = form.cleaned_data
-            user.avatar = request.FILES['avatar']
-            user.save()
-            if check_password(cd['old_passwd'], user.password):
-                if cd['passwd'] == cd['passwd1']:
-                    print('ееее, типо сменил пароль, типа умный да да я')
+            print(cd)
+            if cd['avatar']:
+                user.avatar = request.FILES['avatar']
+                user.save()
+            if cd['old_passwd'] and cd['passwd'] and cd['passwd1']:
+                if check_password(cd['old_passwd'], user.password):
+                    if cd['passwd'] == cd['passwd1']:
+                        print('ееее, типо сменил пароль, типа умный да да я')
+                        user.set_password(cd['passwd'])
+                else:
+                    return HttpResponse('Ошибка пароля')
             else:
-                print('неверный пароль')
+                return redirect('my_p')
     else:
         form = UpdateUserForm()
     return render(request, 'main/profile.html', {'request': request, 'user': user, 'form': form})
 
 
-# TODO: сделать редиректы на нужные страницы
 def user_login(request):
     if request.method == 'POST':
         form = LoginForm(request.POST)
         if form.is_valid():
             cd = form.cleaned_data
-            aes = Aes()
-            key_aes = aes.get_key_aes()
             user = authenticate(username=cd['email'], password=cd['password'])
             if user is not None:
                 if user.is_active:
                     login(request, user)
-                    return HttpResponse('Authenticated successfully')
+                    return redirect('index')
                 else:
                     return HttpResponse('Disabled account')
             else:
-                return HttpResponse('Invalid login')
+                return redirect('index')
     else:
         form = LoginForm()
     return render(request, 'main/login.html', {'form': form})
@@ -68,5 +68,20 @@ def view_another_profile(request, pk):
     except User.DoesNotExist:
         return HttpResponse('дурак?')
 
+    print(user_me.position.access_level)
+    print(user.position.access_level)
 
-    # if user_me.position.access_level
+    if user_me.is_superuser:
+        print('суперюзер(я)')
+        return HttpResponse('батя этого сервера - я')
+
+    elif user.is_superuser:
+        print('суперюзер(не я)')
+        return HttpResponse('батя этого сервера - он')
+
+    elif user_me.position.access_level > user.position.access_level:
+        print('НЕТ ПРАВ!!!')
+        return HttpResponse('У вас недостаточно прав для осуществления')
+    else:
+        print('збс')
+        return HttpResponse('збс')
